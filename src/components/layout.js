3d57334adaf0097915938fc561360fbaf8ef3fd3
Justin Leung <justin.leung.ch@gmail.com>
2020-08-03 11:06:58 -0400

    edit margins, add descriptions

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const ListLink = props => (
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `0.2rem auto`, maxWidth: `35%`, padding: `0 1rem` }}>
+    <div style={{ margin: `0.2rem auto`, maxWidth: `50%`, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.0rem` }}>
         <h1 style={{float:`none`}}>Justin Leung</h1>
         <ul style={{ listStyle: `none`, float: `right` }}>
