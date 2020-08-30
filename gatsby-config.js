1473e1339b9c15703958197fe5097247d1a6ab98
Justin <justin.leung.ch@gmail.com>
2020-08-29 22:13:34 -0400

    Edit SEO title

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,8 +118,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Justin Leung's Website`,
+        short_name: `JustinLeung`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
